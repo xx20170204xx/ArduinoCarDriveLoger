763d83c043d806e77c2788a7cdd40b5c8dc78b63
xx20170204xx <post@kxe.sakura.ne.jp>
2023-03-06 00:56:45 +0900

    追加

---
+++ AndroidPluginProject/ArduinoCarDriveLoger/MySerialServiceLib/src/main/java/jp/ne/sakura/jacobi/myserialservicelib/MyReceiver.java
@@ -0,0 +1,25 @@
+package jp.ne.sakura.jacobi.myserialservicelib;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    public static String buf;
+    public static float waterTemp = 0.0f;
+    public static float oilTemp = 0.0f;
+    public static float oilPress = 0.0f;
+    public static float boostPress = 0.0f;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        buf = intent.getStringExtra(MySerialService.C_INTENT_DEBUGBUF);
+        waterTemp = intent.getFloatExtra(MySerialService.C_INTENT_WATER_TEMP, 0.0f);
+        oilTemp = intent.getFloatExtra(MySerialService.C_INTENT_OIL_TEMP, 0.0f);
+        oilPress = intent.getFloatExtra(MySerialService.C_INTENT_OIL_PRESS, 0.0f);
+        boostPress = intent.getFloatExtra(MySerialService.C_INTENT_BOOST_PRESS, 0.0f);
+    } /* onReceive */
+}
